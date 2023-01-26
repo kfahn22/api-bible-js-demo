e1243b6a0f1bf79c01e7eb149076f7c63ef1a0f6
Kathy <65121394+kfahn22@users.noreply.github.com>
2023-01-26 17:38:54 -0500

    first commit

--- build/js/my_key.js
+++ build/js/my_key.js
@@ -1,1 +1,3 @@
-const API_KEY = `your_own_api_key`; // Fill this in with your own API key from https://portal.api.bible/
+//const API_KEY = `your_own_api_key`; // Fill this in with your own API key from https://portal.api.bible/
+
+const API_KEY = `process.env.API_KEY`;
